21ef01beb689af664040d4f0f0a4fedb3ffa3e2c
Will Moran <will.c.moran@gmail.com>
2024-05-13 11:59:04 -0400

    Updated purchasingAgent script to include company they work for and their phone number

--- scripts/purchasingAgents.js
+++ scripts/purchasingAgents.js
@@ -5,17 +5,19 @@ const businesses = getBusinesses();
 const content = document.querySelector('#content');
 
 export const handlePurchasingAgents = () => {
-    const agents = businesses.map(business => {
-        return business.purchasingAgent
+    const agentInfos = businesses.map(business => {
+        return business
     })
 
     content.innerHTML += `<h2 class='slice'>Purchasing Agents</h2>`
 
-    console.table(agents)
+    console.table(agentInfos)
 
-    agents.forEach(agent => {
+    agentInfos.forEach(agent => {
         content.innerHTML += `<section class='businessInfo'>
-        <h3>${agent.nameFirst} ${agent.nameLast}</h3>
+        <h3>${agent.purchasingAgent.nameFirst} ${agent.purchasingAgent.nameLast}</h3>
+        <h4>${agent.companyName}</h4>
+        <h4>${agent.phoneWork}</h4>
     </section>`
     });
 }
